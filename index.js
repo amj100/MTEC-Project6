0e6ad6b311531eaf776d1fd4eb1e120ed3260edf
Andrew Jacob <amj1108@icloud.com>
2020-01-27 10:35:11 -0700

    add base files

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const port = process.env.PORT || 3000
 const app = express()
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname+'/styles.css'))
 	res.render("index", {title: "test", message: "a message", partialVariable: "[this is a partial variable]"})
 })
 
