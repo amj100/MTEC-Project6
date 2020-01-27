5b86b1bc0acbc52716aaa3c5ace90d7b49727f56
Andrew Jacob <amj1108@icloud.com>
2020-01-27 15:12:32 -0700

    add edit user

--- index.js
+++ index.js
@@ -71,6 +71,30 @@ app.get("/edit_user/:id", async (req, res) => {
 	}
 	res.render("edit_user", {id: id, name: name, email: email, age: age})
 })
+app.post("/edit_user", async (req, res) => {
+	let id = req.body.id
+	let name = req.body.name
+	let email = req.body.email
+	let age = req.body.age
+	let list = {}
+	try {
+		let data = await fs.readFile(path.join(__dirname, "users.json"), "utf8")
+		list = JSON.parse(data)
+		if (id && name && email && age && list[id]) {
+			list[id] = {
+				id: id,
+				name: name,
+				email: email,
+				age: age
+			}
+		}
+		await fs.writeFile(path.join(__dirname, "users.json"), JSON.stringify(list), "utf8")
+	}
+	catch(e) {
+		list = {}
+	}
+	res.redirect("/users")
+})
 
 app.use(async (req, res) => {
 	res.redirect("/")
